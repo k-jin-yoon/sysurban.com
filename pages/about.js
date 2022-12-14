6e6f543cec397160452f7aaedd8d06d91a9ecc8a
kjin <kjin@sysurban.com>
2022-12-14 16:38:50 +0900

    1차 커밋

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Seo from "../components/Seo";
+
+export default function About(){
+    return (
+        <div className="container">
+            <Seo title="About" />
+            <h1>"About Sysurban!!!"</h1>
+            <div>
+                시스어반에서는
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eligendi quos mollitia, dolore ab veniam, dignissimos earum voluptatem praesentium illum sequi laboriosam ducimus quo? Nostrum recusandae dicta expedita illo sit ipsam?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Neque harum a aut omnis eum mollitia, dolores perspiciatis illum! Maxime voluptatem fugiat ex recusandae dolorum repellendus, ducimus tempore impedit aliquid unde!
+            </div>
+        </div>
+    );
+}
